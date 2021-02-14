c6c5f2bb569bef79c5bfdd6f182b66668d6f290b
ShreeshaN <nshreesha2011@gmail.com>
2021-02-14 14:28:48 -0500

    mit data processor

---
+++ covid_19/configs/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+@created on: 2/11/21,
+@author: Shreesha N,
+@version: v0.0.1
+@system name: badgod
+Description:
+
+..todo::
+
+"""
---
+++ covid_19/datagen/data_processor.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+@created on: 2/11/21,
+@author: Shreesha N,
+@version: v0.0.1
+@system name: badgod
+Description:
+
+..todo::
+
+"""
+import json
+import argparse
+import pandas as pd
+
+from covid_19.utils.data_utils import stratified_train_test_split
+from covid_19.utils.class_utils import AttributeDict
+from covid_19.datagen.audio_feature_extractors import preprocess_data
+
+
+class DataProcessor:
+    def __init__(self, args):
+        self.coswara_datapath = args.coswara_datapath
+        self.mit_datapath = args.mit_datapath
+        self.mit_audiopath = args.mit_audiopath
+        self.normalize_while_creating = args.normalise_while_creating
+        self.sample_size_in_seconds = args.sample_size_in_seconds
+        self.sampling_rate = args.sampling_rate
+        self.overlap = args.overlap
+        self.data_processing_method = args.data_processing_method
+
+    def coswara_processor(self):
+        pass
+
+    def mit_processor(self):
+        columns_to_consider = ['id', 'diagnosis']
+        data = pd.read_csv(self.mit_datapath)[columns_to_consider]
+        data = data.dropna()
+        X, y = data['id'].values, data['diagnosis'].map({'Yes': 1, 'No': 0}).values
+        train_index, test_index = stratified_train_test_split(X, y, test_size=0.3,
+                                                              random_state=10)
+        data[data.index.isin(train_index)].to_csv(self.mit_audiopath + '/train_data.csv')
+        data[data.index.isin(test_index)].to_csv(self.mit_audiopath + '/test_data.csv')
+        train_X, test_X, train_y, test_y = X[train_index], X[test_index], y[train_index], y[test_index]
+        # import librosa, glob
+        # for file in train_X:
+        #     print(file)
+        #     for audiofile in glob.glob(self.mit_audiopath + file + '/*.wav'):
+        #         _, _ = librosa.load(audiofile)
+        preprocess_data(self.mit_audiopath, train_X, train_y, self.normalize_while_creating,
+                        self.sample_size_in_seconds, self.sampling_rate, self.overlap, self.data_processing_method)
+
+    def run(self):
+        self.mit_processor()
+        # self.coswara_processor()
+
+
+def parse():
+    parser = argparse.ArgumentParser(description="covid19_data_processor")
+    parser.add_argument('--configs_file', type=str)
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = parse().__dict__
+    configs = json.load(open(args['configs_file']))
+    configs = {**configs, **args}
+    configs = AttributeDict(configs)
+    processor = DataProcessor(configs)
+    print(json.dumps(configs, indent=4))
+    processor.run()
